8938b05a85249081866912c526ed05c9c43e2cb6
Patrick Ranval <pat.ranval@gmail.com>
2024-03-10 18:44:50 -0500

    Encoding entire user to JWT to avoid second API call
    Dubious....

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
-      token = jwt_encode(user_id: user.id)
+      token = jwt_encode(user_id: user.id, first_name: user.first_name, last_name: user.last_name, email: user.email)
       render json: { token: token }, status: :ok
     else
       render json: { error: "Unauthorized" }, status: :unauthorized
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  
   def create
     user = User.new(user_params)
     if user.save
@@ -8,9 +10,33 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    render json: @user, status: :ok
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { error: "Some kinda fail" }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: nil, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
     params.permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
